5c335482ea6d285163d2c52eb67368b962d55356
Fernando José Gómez Hernández <62573930+Fergomez0901@users.noreply.github.com>
2021-03-16 16:43:36 -0600

    Clase16Mar

---
+++ Paradigmas_POO/ClasesYOBjetos/ClasesYOBjetos/Cocina.cs
@@ -0,0 +1,40 @@
+﻿using System; 
+namespace ClasesYOBjetos
+{
+    public class Cocina
+    {
+        //Atributos
+        private int quemadores; 
+        private string color; 
+        private string marca; 
+        private string combustible;
+        private bool tieneHorno; 
+        
+        //Constructores
+        public Cocina(int pQue, string pCol, string pMar, string pCom, bool pTH) 
+        {
+                 quemadores = pQue; 
+                 color = pCol; 
+                 marca = pMar; 
+                 combustible = pCom; 
+                 tieneHorno = pTH; 
+        }
+        
+        //Métodos
+        public void encender()
+        { 
+            Console.WriteLine("Encendiendo...");
+        }
+        public void apagar()
+        {
+            Console.WriteLine("Apagando...");
+        }
+        public void hornear()
+        {
+            if(tieneHorno)
+                Console.WriteLine("Horneando...");
+            else
+                Console.WriteLine("Error..."); 
+        }   
+    }
+}
---
+++ Paradigmas_POO/ClasesYOBjetos/ClasesYOBjetos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ClasesYOBjetos
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Cocina cocina01 = new Cocina(4, "azul", "Samsung", "electrica", true);
+            cocina01.encender();
+            cocina01.hornear();
+            cocina01.apagar(); 
+        }
+    }
+}
